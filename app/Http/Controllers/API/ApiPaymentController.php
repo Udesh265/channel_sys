f70eef080eb5d17d8a4d6b8818e91642ecc0fd5e
Udesh265 <31616257+Udesh265@users.noreply.github.com>
2021-03-18 23:50:18 +0530

    finish online appointment part edit

---
+++ app/Http/Controllers/API/ApiPaymentController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Payment;
+use Illuminate\Http\Request;
+
+class ApiPaymentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+   public function submit_payment($pay_id){
+        $data = Payment::find($pay_id);
+
+        $data->update([
+            'payment_status' => 'Confirm',
+        ]);
+
+        if(is_null($data)){
+
+            return response()->json(['msg'=>'faild to update payment'],400);
+        }
+
+            return response()->json(['msg'=>'Payment Successfull'],200);
+
+   }
+}
